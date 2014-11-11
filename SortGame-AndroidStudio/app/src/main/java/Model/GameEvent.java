67cc9a0161a90ea546837cb58b40e6999daf8747
Jeton <jsinoimeri@yahoo.ca>
2014-11-11 02:05:16 -0500

    Version 1.1 of GameEvent.java

--- SortGame-AndroidStudio/app/src/main/java/Model/GameEvent.java
+++ SortGame-AndroidStudio/app/src/main/java/Model/GameEvent.java
@@ -2,17 +2,31 @@ package Model;
 
 import java.util.EventObject;
 
+
 /**
+ * GameEvent is an EventObject.
+ *
+ * @see java.util.EventObject
  *
  * @author Jeton Sinoimeri
- * @version 1.0
+ * @version 1.1
  * @since 2014-11-10
  *
  */
+
 public class GameEvent extends EventObject
 {
+
+    /**
+     * Constructor for the GameEvent class
+     *
+     * @param arg0 -> Object representing the source of
+     *                the Event
+     */
+
     public GameEvent(Object arg0)
     {
         super(arg0);
     }
+
 }
