271d5c33d282aac09a5518b3444a1c3ba0cc3407
Jeton <jsinoimeri@yahoo.ca>
2014-11-11 02:03:13 -0500

    Version 1.0 of TimerTest.java

---
+++ SortGame-AndroidStudio/app/src/androidTest/java/Model/TimerTest.java
@@ -0,0 +1,57 @@
+package Model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * JUnit test for Timer class
+ *
+ * @author Jeton Sinoimeri
+ * @version 1.0
+ * @since 2014-11-11
+ *
+ */
+
+public class TimerTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testOnFinish() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testOnTick() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testPause() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testResume() throws Exception
+    {
+
+    }
+}
