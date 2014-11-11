1603b043840affce938daf07d11171a7087f43d0
Jeton <jsinoimeri@yahoo.ca>
2014-11-11 02:06:16 -0500

    Version 1.1 of Timer.java

--- SortGame-AndroidStudio/app/src/main/java/Model/Timer.java
+++ SortGame-AndroidStudio/app/src/main/java/Model/Timer.java
@@ -3,19 +3,64 @@ package Model;
 import android.os.CountDownTimer;
 
 /**
+ * Timer counts down from an initial time
+ * to 0 in intervals of milliseconds
+ *
+ * @see android.os.CountDownTimer
  *
  * @author Jeton Sinoimeri
- * @version 1.0
+ * @version 1.1
  * @since 2014-11-10
  *
  */
+
 public class Timer extends CountDownTimer
 {
+
+    /**
+     * Long constant representing the interval in which
+     * the time will be decreased by.
+     *
+     */
+
     private static final Long INTERVAL = 1000L;
-    private long timerLeft;
+
+
+
+
+    /**
+     * Long instance representing the amount of time
+     * left in the session.
+     *
+     */
+
+    private long timeLeft;
+
+
+    /**
+     * CountDownTimer instance representing the timer class
+     *
+     */
+
     private CountDownTimer timer;
 
+
+    /**
+     * Game instance representing the game which is being
+     * played.
+     *
+     */
+
     private Game game;
+
+
+    /**
+     * Constructor for the Timer class
+     *
+     * @param millisecondsInitTime -> Long representing the initial time in milliseconds
+     * @param game -> Game instance representing the game which is being played
+     *
+     */
 
     public Timer(long millisecondsInitTime, Game game)
     {
@@ -23,27 +68,54 @@ public class Timer extends CountDownTimer
         this.game = game;
     }
 
+
+    /**
+     * @see android.os.CountDownTimer
+     *
+     */
+
     @Override
     public void onFinish()
     {
         this.game.timeOut();
     }
 
+    /**
+     * @see android.os.CountDownTimer
+     *
+     * @param millisUntilFinished -> Long representing the amount of time
+     *                            left until the timer finishes in
+     *                            milliseconds.
+     *
+     */
+
     @Override
     public void onTick(long millisUntilFinished)
     {
-        this.timerLeft = millisUntilFinished;
-
+        this.timeLeft = millisUntilFinished;
     }
+
+
+    /**
+     * Cancels the countdown timer when pause is called
+     *
+     */
 
     public void pause()
     {
         this.cancel();
     }
 
+
+    /**
+     * Resumes the countdown timer using the previous
+     * paused time as the starting value.
+     *
+     */
+
     public void resume()
     {
-        this.timer = new Timer(this.timerLeft, game);
+        this.timer = new Timer(this.timeLeft, this.game);
         this.timer.start();
     }
 
