d2f6df2a7b5bc76a40beff0a1437bd01918f161c
jsinoimeri <jsinoimeri@yahoo.ca>
2014-12-26 01:02:57 -0500

    Version 1.0 of DeckTest.java

---
+++ SortGame-AndroidStudio/app/src/androidTest/java/Model/DeckTest.java
@@ -0,0 +1,172 @@
+package Model;
+
+import junit.framework.TestCase;
+
+import java.util.Stack;
+
+/**
+ * JUnit testing for Deck class.
+ *
+ * @see junit.framework.TestCase
+ *
+ * @author Jeton Sinoimeri
+ * @version 1.0
+ * @since 2014-12-25
+ *
+ */
+
+public class DeckTest extends TestCase
+{
+
+    /**
+     * deck: Deck instance representing the deck
+     *
+     */
+
+    private Deck deck;
+
+
+    /**
+     * game: Game instance representing the game
+     *
+     */
+
+    private Game game;
+
+
+    /**
+     * gameEvent: GameEvent representing the game event
+     *
+     */
+
+    private GameEvent gameEvent;
+
+
+
+
+    /**
+     * @see junit.framework.TestCase
+     *
+     * @throws Exception indicating that there was an exception
+     *         in initializing of the fields and resources
+     *
+     */
+
+    public void setUp() throws Exception
+    {
+        super.setUp();
+
+        this.deck = new Deck();
+        this.game = new Game(new Player(), new Multiplier(), this.deck);
+        this.gameEvent = new GameEvent(this.game);
+    }
+
+
+    /**
+     * @see junit.framework.TestCase
+     *
+     * @throws Exception indicating that there was an exception
+     *         in deallocating of the fields and resources
+     *
+     */
+
+    public void tearDown() throws Exception
+    {
+        super.tearDown();
+
+        this.deck = null;
+        this.game = null;
+        this.gameEvent = null;
+
+    }
+
+
+    /**
+     * Tests the deckSize method of the Deck class
+     *
+     */
+
+    public void testDeckSize()
+    {
+        assertEquals(0, this.deck.deckSize());
+
+    }
+
+
+    /**
+     * Tests the getDeck method of the Deck class
+     *
+     */
+
+    public void testGetDeck()
+    {
+        assertTrue(this.deck.getDeck() instanceof Stack);
+
+    }
+
+
+    /**
+     * Tests roundsOver method of the Deck class
+     *
+     */
+
+    public void testRoundsOver()
+    {
+        this.deck.livesFinish(this.gameEvent);
+
+        for (int i = 14; i > -1; i--)
+        {
+            this.deck.roundsOver(this.gameEvent);
+            assertEquals(i, this.deck.deckSize());
+
+        }
+
+    }
+
+
+    /**
+     * Tests the correctMatch method of the Deck class
+     *
+     */
+
+    public void testCorrectMatch()
+    {
+
+    }
+
+
+    /**
+     * Tests the timeOut method of the Deck class
+     *
+     */
+
+    public void testTimeOut()
+    {
+        this.deck.livesFinish(this.gameEvent);
+
+        for (int i = 14; i > -1; i--)
+        {
+            this.deck.timeOut(this.gameEvent);
+            assertEquals(i, this.deck.deckSize());
+
+        }
+
+
+    }
+
+
+    /**
+     * Tests the livesFinished method of the Deck class
+     *
+     */
+
+    public void testLivesFinish()
+    {
+        assertEquals(0, this.deck.deckSize());
+
+        this.deck.livesFinish(this.gameEvent);
+
+        assertEquals(15, this.deck.deckSize());
+
+    }
+}
