3eb3ece26ac3641bfbd9f4808bc075e17d20094d
Jeton Sinoimeri <jsinoimeri@yahoo.ca>
2015-03-06 18:49:42 -0500

    Update GameManager.java

--- SortGame-AndroidStudio/app/src/main/java/com/example/owner/gameproject/GameManager.java
+++ SortGame-AndroidStudio/app/src/main/java/com/example/owner/gameproject/GameManager.java
@@ -8,35 +8,32 @@ import android.graphics.Canvas;
  * @author Robert
  * @version version_number
  * @since 2015-02-27
- * Created by Robert on 27/02/2015.
  */
 public class GameManager{
 
     private Card card;
-
     private GameView view;
 
+    //Variables representing screen center coordinates in pixels
     public static float centerX;
     public static float centerY;
-    public static float scale;
-    public static GameManager instance = null;
-
+    
+    //Variable representing X axis scale offset (float->pixel)
+    public static float scaleX;
+    
+    //Boolean which is true only on first draw
     private boolean start = true;
-
 
     public GameManager(GameView gameview){
         card = new Card();
         view = gameview;
-        if(instance ==  null){
-            instance = this;
-        }
     }
 
     public void draw(){
         if(start){
             centerX = view.getWidth()/2;
             centerY = view.getHeight()/2;
-            scale = centerX / 2;
+            scaleX = centerX / 2;
             start = false;
         }
         card.draw();
