b62ad62a32b99bf246771dad685b927e8aa8897c
VarunSriram <varun_sriram_5@hotmail.com>
2015-03-20 21:42:03 -0400

    Set the position of the card made it a bit smaller

--- SortGame-AndroidStudio/app/src/main/java/com/example/owner/gameproject/Card.java
+++ SortGame-AndroidStudio/app/src/main/java/com/example/owner/gameproject/Card.java
@@ -70,12 +70,12 @@ public class Card
     {
         color = new Paint();
 
-        float offsetFromCentre = SIZE * GameView.WIDTH/4;
+        float offsetFromCentre = SIZE * GameView.WIDTH/6;
 
         xCoordinate1 = GameView.WIDTH/2 - offsetFromCentre;
-        yCoordinate1 = GameView.HEIGHT/2 - offsetFromCentre;
+        yCoordinate1 = GameView.HEIGHT*0.625f - offsetFromCentre;
         xCoordinate2 = GameView.WIDTH/2 + offsetFromCentre;
-        yCoordinate2 = GameView.HEIGHT/2 + offsetFromCentre;
+        yCoordinate2 = GameView.HEIGHT*0.625f + offsetFromCentre;
 
         this.generateNewColor();
     }
