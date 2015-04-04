9c4c12fc81b543a42410ec5a2ab6a1db5e732cad
VarunSriram <varun_sriram_5@hotmail.com>
2015-04-03 22:06:08 -0400

    Added functionality for crd animations

--- SortGame-AndroidStudio/app/src/main/java/com/example/owner/gameproject/GameBoard.java
+++ SortGame-AndroidStudio/app/src/main/java/com/example/owner/gameproject/GameBoard.java
@@ -1,6 +1,7 @@
 package com.example.owner.gameproject;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 
 /**
@@ -18,6 +19,7 @@ public class GameBoard
 {
     /** RADIUS constant float value representing the radius of the circle.*/
     private static final float RADIUS = GameView.WIDTH * (95.0f/720.0f);
+
 
     /**
      * redCircleColor android.graphics.Paint instance representing color red.
@@ -78,9 +80,9 @@ public class GameBoard
      * @param y float value representing the y-coordinate in pixels.
      * @return boolean value representing whether or not the color red is touched.
      */
-    private boolean redTouched(float x, float y)
+    public boolean redTouched(float x, float y)
     {
-        return (x <= (330f/1080) * GameView.WIDTH) && (y <= (875f/1920) * GameView.HEIGHT && y >= (550f/1920) * GameView.HEIGHT);
+        return (x <= (330f/1080) * GameView.WIDTH) && (y <= (820f/1920) * GameView.HEIGHT && y >= (550f/1920) * GameView.HEIGHT);
     }
     /**
      * Checks if the color green is touched.
@@ -89,9 +91,9 @@ public class GameBoard
      * @param y: float value representing the y-coordinate in pixels.
      * @return boolean value representing whether or not the color green is touched.
      */
-    private boolean greenTouched(float x, float y)
+    public boolean greenTouched(float x, float y)
     {
-        return (x >= (745f/1080)*GameView.WIDTH) && (y <= (875f/1920)*GameView.HEIGHT && y >= (550f/1920) * GameView.HEIGHT);
+        return (x >= (745f/1080)*GameView.WIDTH) && (y <= (820f/1920)*GameView.HEIGHT && y >= (550f/1920) * GameView.HEIGHT);
     }
 
     /**
@@ -101,7 +103,7 @@ public class GameBoard
      * @param y: float value representing the y-coordinate in pixels.
      * @return boolean value representing whether or not the color blue is touched.
      */
-    private boolean blueTouched(float x, float y)
+    public boolean blueTouched(float x, float y)
     {
         return (x <= (330f/1080)*GameView.WIDTH) && (y >= (1580f/1920)*GameView.HEIGHT);
     }
@@ -113,7 +115,7 @@ public class GameBoard
      * @param y: float value representing the y-coordinate in pixels.
      * @return boolean value representing whether or not the color purple is touched.
      */
-    private boolean purpleTouched(float x, float y)
+    public boolean purpleTouched(float x, float y)
     {
         return (x >= (745f/1080)*GameView.WIDTH) && (y >= (1580f/1920)*GameView.HEIGHT);
     }
@@ -126,6 +128,7 @@ public class GameBoard
      */
     public void draw(Canvas canvas)
     {
+
         canvas.drawRect(0f, 0f, GameView.WIDTH, (100f/1080f)*GameView.HEIGHT, this.darkBlueTopBarColor);
         canvas.drawCircle((175f/1080)*GameView.WIDTH,0.9f*GameView.HEIGHT, RADIUS, this.blueCircleColor);
         canvas.drawCircle((905f/1080)*GameView.WIDTH,0.9f*GameView.HEIGHT, RADIUS, this.purpleCircleColor);
@@ -133,4 +136,7 @@ public class GameBoard
         canvas.drawCircle((175f/1080)*GameView.WIDTH, 0.35f*GameView.HEIGHT, RADIUS, this.redCircleColor);
         canvas.drawCircle((905f/1080)*GameView.WIDTH, 0.35f*GameView.HEIGHT, RADIUS, this.greenCircleColor);
     }
+
+    //0.162
+    //0.838
 }
